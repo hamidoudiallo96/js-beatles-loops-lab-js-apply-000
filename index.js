1d1db4ed3279c64a9e5524f3695790b9ebc1f8d3
Hamidou Diallo <dio.hamidou@gmail.com>
2019-08-28 06:33:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // add solution here
+const theBeatlesPlay = (arr1,arr2) =>{
+  let newArr = []
+  for(let i=0;i<arr1.length; i++){
+    newArr.push(`${arr1[i]} plays ${arr2[i]}`)
+  }
+  return newArr;
+}
+
+const johnLennonFacts = arr =>{
+  let newArr = []
+  let i = 0
+  while(i<arr.length){
+    
+  }
+}
